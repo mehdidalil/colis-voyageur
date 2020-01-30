950d26d40b43cd71a00bc63d2adadb0e096b798a
mayday <mdalil@student.42.fr>
2020-01-30 19:08:15 +0100

    Updates

---
+++ precache-manifest.699c62ec10499e7ad223de06ee969abe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49625f9ad79b897fdbabc6b85416606b",
+    "url": "/colis-voyageur/index.html"
+  },
+  {
+    "revision": "8ff31cc0055f2405a229",
+    "url": "/colis-voyageur/static/js/2.0aaddd08.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/colis-voyageur/static/js/2.0aaddd08.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a853999ab82fcc75dbbb",
+    "url": "/colis-voyageur/static/js/main.58fcf6e3.chunk.js"
+  },
+  {
+    "revision": "521582820f505573a9a9",
+    "url": "/colis-voyageur/static/js/runtime-main.c464819d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/colis-voyageur/precache-manifest.d6fb1924020beaaa99669530f1378b01.js"
+  "/colis-voyageur/precache-manifest.699c62ec10499e7ad223de06ee969abe.js"
 );
 
 self.addEventListener('message', (event) => {
