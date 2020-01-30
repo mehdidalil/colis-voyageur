ed77ad5c3caf04e3a6a3d5ec27dc0a6d501e62d5
mayday <mdalil@student.42.fr>
2020-01-30 18:59:45 +0100

    Updates

---
+++ precache-manifest.3bbf05233c171785766beb578e109ae0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e56be72f8be77bfd974279e88dd2fc14",
+    "url": "/colis-voyageur/index.html"
+  },
+  {
+    "revision": "7ff6f9226335195716bb",
+    "url": "/colis-voyageur/static/js/2.2eecd621.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/colis-voyageur/static/js/2.2eecd621.chunk.js.LICENSE"
+  },
+  {
+    "revision": "707676180adc8c94591d",
+    "url": "/colis-voyageur/static/js/main.405ee42e.chunk.js"
+  },
+  {
+    "revision": "521582820f505573a9a9",
+    "url": "/colis-voyageur/static/js/runtime-main.c464819d.js"
+  }
+]);
